fdbd24c4bd243d7e79185c6d50272a4431010aed
Luwu <renan.ap.almeida@gmail.com>
2025-06-26 14:38:46 -0300

    feat: custom dataset class

--- utils/loader.py
+++ utils/loader.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import torch
+from torch.utils.data import Dataset, DataLoader
+
 
 def load_data():
     essays = pd.read_csv("./data/essay-br.csv")
@@ -8,3 +12,34 @@ def load_data():
 
 def categorize_score(score):
     return score // 40
+
+
+#Torch Custom Dataset
+class TextClassificationDataset(Dataset):
+    def __init__(self, embeddings, labels):
+        self.embeddings = embeddings
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        return self.embeddings[idx], self.labels[idx]
+    
+
+def embeddings_into_dataloader(embeddings, labels, batch_size=4):
+    X_train, X_test, y_train, y_test = train_test_split(embeddings, labels, test_size=0.2, random_state=42)
+
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.long)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_test = torch.tensor(y_test, dtype=torch.long)
+
+    train_dataset = TextClassificationDataset(X_train, y_train)
+    test_dataset = TextClassificationDataset(X_test, y_test)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
